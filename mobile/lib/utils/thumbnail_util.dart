d2e49ed26150571bcc4c0f01c649ae51d1ad7d61
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-06-26 22:55:11 -0400

    wip: use translation strings

--- mobile/lib/utils/thumbnail_util.dart
+++ mobile/lib/utils/thumbnail_util.dart
@@ -1,21 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:immich_mobile/entities/exif_info.entity.dart';
-import 'package:intl/intl.dart';
 
-String getAltText(ExifInfo? exifInfo, DateTime? fileCreatedAt) {
+String getAltText(ExifInfo? exifInfo, DateTime fileCreatedAt) {
   if (exifInfo?.description != null && exifInfo!.description!.isNotEmpty) {
     return exifInfo!.description!;
   }
 
-  String altText = 'Image taken';
+  bool hasLocation = exifInfo?.city != null && exifInfo?.country != null;
+  String date = DateFormat.yMMMMd().format(fileCreatedAt);
 
-  if (exifInfo?.city != null && exifInfo?.country != null) {
-    altText += ' in ${exifInfo!.city}, ${exifInfo.country}';
+  if (hasLocation) {
+    String location = '${exifInfo!.city}, ${exifInfo.country}';
+    return 'thumbnail_alt_text_date_and_location'.tr(args: [location, date]);
   }
 
-  if (fileCreatedAt != null) {
-    String date = DateFormat.yMMMMd().format(fileCreatedAt);
-    altText += ' on $date';
-  }
-
-  return altText;
+  return 'thumbnail_alt_text_date'.tr(args: [date]);
 }
